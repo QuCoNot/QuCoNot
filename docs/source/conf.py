64cf3f2313273b8d2263787e509846a17e9394b0
ankit_linux <ankit27.kh@gmail.com>
2022-11-29 17:12:37 +0530

    change theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,22 +33,32 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.inheritance_diagram",
     "sphinx.ext.viewcode",
-    #"sphinxcontrib.bibtex",
+    "sphinxcontrib.bibtex",
     "sphinx.ext.graphviz",
     "sphinx.ext.intersphinx",
     "sphinx_automodapi.automodapi",
     "sphinx_copybutton",
     "sphinx.ext.duration",
-    #"m2r2",
+    "sphinx_togglebutton",
+    "m2r2",
 ]
 templates_path = ['_templates']
 exclude_patterns = []
 numpydoc_show_class_members = False
 autosummary_generate = True
+bibtex_bibfiles = ['refs.bib']
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+#html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
 html_static_path = ['_static']
+html_theme_options = {
+    "repository_url": "https://github.com/QuCoNot/QuCoNot",
+    "use_repository_button": True,
+    "use_issues_button": True,
+}
+html_logo = "logo.png"
+html_title = "QuCoNot"
