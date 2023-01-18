2185b287a347f76273141f804fd910ab83e0253b
Handy Kurniawan <handykhandy@gmail.com>
2023-01-18 16:14:24 +0200

    update implementation

--- quconot/implementations/__init__.py
+++ quconot/implementations/__init__.py
@@ -1,13 +1,21 @@
+from .mct_barenco_74_dirty import MCTBarenco74Dirty
+from .mct_barenco_75_dirty import MCTBarenco75Dirty
 from .mct_n_qubit_decomposition import MCTNQubitDecomposition
 from .mct_no_auxiliary import MCTNoAuxiliary
 from .mct_no_auxiliary_relative import MCTNoAuxiliaryRelative
 from .mct_parallel_decomposition import MCTParallelDecomposition
+from .mct_recursion import MCTRecursion
 from .mct_vchain import MCTVChain
+from .mct_vchain_dirty import MCTVChainDirty
 
 __all__ = [
+    "MCTBarenco74Dirty",
+    "MCTBarenco75Dirty",
     "MCTNQubitDecomposition",
     "MCTNoAuxiliary",
     "MCTNoAuxiliaryRelative",
     "MCTParallelDecomposition",
+    "MCTRecursion",
     "MCTVChain",
+    "MCTVChainDirty",
 ]
