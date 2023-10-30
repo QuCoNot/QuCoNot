0774ae39ca5517e91c217ae2cc82bb6fc8636270
Adam Glos <adam@algorithmiq.fi>
2023-10-30 13:12:49 +0100

    lru-cached loading matrix

--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,9 @@
+from functools import lru_cache
+
 import numpy as np
 
 
-def load_matrix(mode, controls_no):
+@lru_cache()
+def load_matrix(mode: str, controls_no: int):
     container = np.load("./tests/ref_matrices/mct_" + mode + ".npz")
     return container["arr_" + str(controls_no - 1)]
