08f40537fde152b2016a9cf561c84950199588b6
Adam Glos <adam@algorithmiq.fi>
2023-10-30 13:08:55 +0100

    chore: unnecessary npz files removed

--- tests/utils.py
+++ tests/utils.py
@@ -2,5 +2,5 @@ import numpy as np
 
 
 def load_matrix(mode, controls_no):
-    container = np.load("./tests/mct_matrices/mct_" + mode + ".npz")
+    container = np.load("./tests/ref_matrices/mct_" + mode + ".npz")
     return container["arr_" + str(controls_no - 1)]
