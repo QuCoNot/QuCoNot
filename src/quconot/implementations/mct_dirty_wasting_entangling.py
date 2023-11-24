4437ac653a909dec7bcadf022869e27664fb3866
Adam <aglos@iitis.pl>
2023-11-24 14:34:18 +0100

    missing implementations added, jupyter notebook written

---
+++ src/quconot/implementations/mct_dirty_wasting_entangling.py
@@ -0,0 +1,72 @@
+# Quconot/quconot/implementations/mct_dirty_wasted_entangling.py
+#
+# Authors:
+#  - Adam Glos
+# Apply the implementation from (...)
+
+
+from typing import List
+
+from qiskit import QuantumCircuit
+
+from .mct_base import MCTBase
+
+
+class MCTDirtyWastingEntangling(MCTBase):
+    def __init__(self, controls_no: int, **kwargs) -> None:
+        if controls_no < 3:
+            raise ValueError("Number of controls must be >= 3 for this implementation")
+        self._n = controls_no
+        self._circuit: QuantumCircuit = None
+
+    @classmethod
+    def verify_mct_cases(
+        self,
+        controls_no: int,
+        max_auxiliary: int,
+        relative_phase: bool = False,
+        clean_acilla: bool = True,
+        wasted_auxiliary: bool = False,
+        separable_wasted_auxiliary: bool = False,
+    ) -> List["MCTBase"]:
+        """Generate all possible MCT implementation satisfying the requirements
+
+        relative_phase: true / false (D)
+        clean_auxiliary: true (D) / false
+        wasted_auxiliary: true / false (D)
+        separable_wasted_auxiliary: true / false (D), requires wasted_auxiliary set to True
+
+        :return: a quantum circuit
+        :rtype: QuantumCircuit
+        """
+        if max_auxiliary != controls_no - 1:
+            return []  # if max_auxiliary allowed is incorrect - no representation given
+        else:
+            return [MCTDirtyWastingEntangling(controls_no)]  # only one available
+
+    def generate_circuit(self) -> QuantumCircuit:
+        """Return a QuantumCircuit implementation
+
+        :return: a quantum circuit
+        :rtype: QuantumCircuit
+        """
+        qc = QuantumCircuit(self._n + 1 + self.num_auxiliary_qubits())
+
+        ancilla_ind = self._n + 1
+
+        qc.rccx(self._n - 1, qc.num_qubits - 1, self._n)
+        for i in range(self.num_auxiliary_qubits() - 2, -1, -1):
+            qc.rccx(1 + i, ancilla_ind + i, ancilla_ind + i + 1)
+        qc.rccx(0, 1, ancilla_ind)
+        for i in range(0, self.num_auxiliary_qubits() - 1):
+            qc.rccx(1 + i, ancilla_ind + i, ancilla_ind + i + 1)
+        qc.rccx(self._n - 1, qc.num_qubits - 1, self._n)
+        return qc
+
+    def num_auxiliary_qubits(self):
+        """Return number of auxiliary qubits
+
+        :return: number of auxiliary qubits
+        :rtype: int
+        """
+        return self._n - 1
---
+++ tests/test_mct_dirty_wasting_entangling.py
@@ -0,0 +1,46 @@
+from typing import Dict, Type
+
+import numpy as np
+import pytest
+
+from quconot.implementations.mct_base import MCTBase
+from quconot.implementations.mct_dirty_wasted_entangling import (
+    MCTDirtyWastingEntangling,
+)
+from tests.test_mct_base import BaseTestMCT
+
+
+class TestMCTDirtyWastingEntangling(BaseTestMCT):
+    _matrix_dict: Dict[int, np.ndarray] = {}
+    _ref_matrices: Dict[int, np.ndarray] = {}
+    _controls_no_list = [5]
+
+    _expected_classes: Dict[str, bool] = {
+        "SCNW": False,
+        "RCNW": False,
+        "SDNW": False,
+        "RDNW": False,
+        "SCWE": True,
+        "SCWS": False,
+        "RCWS": False,
+        "SDWE": True,
+        "SDWS": False,
+        "RDWS": False,
+    }
+
+    @property
+    def _get_class_name(self) -> Type[MCTBase]:
+        return MCTDirtyWastingEntangling
+
+    def test_init(self):
+        with pytest.raises(
+            ValueError, match="Number of controls must be >= 3 for this implementation"
+        ):
+            MCTDirtyWastingEntangling(1)
+
+        try:
+            MCTDirtyWastingEntangling(3)
+        except Exception:
+            assert (
+                False
+            ), "object MCTDirtyWastingEntangling(3) was not created, but it should be"
